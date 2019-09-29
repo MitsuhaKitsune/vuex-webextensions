c014c94f6e359501e60bc3c1fee03a3a77897db8
Mitsuha Kitsune <kitsunemitsuha@gmail.com>
2019-09-29 02:37:28 +0200

    Fix es-lint rule -.-"

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'array-bracket-newline': 'error',
     'array-bracket-spacing': 'error',
     'array-callback-return': 'error',
-    'array-element-newline': 'error',
+    'array-element-newline': ['error', { minItems: 10 }],
     'arrow-body-style': 'error',
     'arrow-parens': 'error',
     'arrow-spacing': 'error',
