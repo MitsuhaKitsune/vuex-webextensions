4dabc0f7174e4a56a3b60deeea5adf30164c90ce
Mitsuha Kitsune <kitsunemitsuha@gmail.com>
2019-12-09 04:10:29 +0100

    Update dependencies and use rollup to build into one single file

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
+export default [
+  {
+    input: './src/index.js',
+    output: {
+      file: './dist/index.js',
+      format: 'iife',
+      name: 'index',
+      sourcemap: true
+    },
+    external: ['vuex'],
+    plugins: [babel(), uglify()]
+  }
+];
